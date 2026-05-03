351f6a9a0088aaed912be9fe9cba44af18951d32
Shreyas Mayya <smayya337@gmail.com>
2026-05-03 12:49:59 -0400

    add new constitution

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -18,7 +18,7 @@
         <h2>Supporting ACM @ UVA</h2>
     <p>ACM is a 501(c) nonprofit organization. Our funds go towards hosting great events for students, including HSPC.</p>
     <h2>Bylaws</h2>
-    <p>The chapter bylaws are available <a href="/ACM 2025 Bylaws.pdf">here</a>.</p>
+    <p>The chapter bylaws are available <a href="/ACM at UVA 2026-2027 Constitution.pdf">here</a>.</p>
     </div>
     </div>
     <div>
@@ -33,4 +33,4 @@
     @endforeach
     @endif
     </div>
-@endsection 
+@endsection
